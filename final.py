0ef696742c7f92e308b12437d24b6f89ca24678e
FranzEAV <franz.alejov.0159@gmail.com>
2024-01-12 11:44:43 -0500

    creacion de archivo final para descarga de dataset y sql para importar dataset

---
+++ final.py
@@ -0,0 +1,27 @@
+import json
+import zipfile
+import os
+
+api_token={"username":"franzalejo","key":"0f2dbb224442e6addf598e2c21aa08eb"}
+
+with open("C:/Users/Usuario/.kaggle/kaggle.json","w") as file:
+    json.dump(api_token, file)
+
+location = "C:/Users/Usuario/Documents/alfa/dataset"
+
+if not os.path.exists(location):
+    os.mkdir(location)
+else:
+    for root, dirs, files in os.walk(location, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+
+os.system("kaggle datasets download -d vishakhdapat/price-of-used-toyota-corolla-cars -p C:/Users/Usuario/Documents/alfa/dataset")            
+
+os.chdir(location)
+for file in os.listdir():
+    zip_ref = zipfile.ZipFile(file,"r")
+    zip_ref.extractall()
+    zip_ref.close()
